24f5f1a3eee10e3d5efe934ebdbec7956fa39d0e
daniel dabrowski <agrcom@yahoo.pl>
2018-02-26 22:29:52 +0100

    domain model

---
+++ src/main/java/MarkReadMessageCommand.java
@@ -0,0 +1,12 @@
+import org.axonframework.commandhandling.TargetAggregateIdentifier;
+
+public class MarkReadMessageCommand {
+
+    @TargetAggregateIdentifier
+    private String id;
+
+    public MarkReadMessageCommand(String id) {
+        this.id = id;
+    }
+
+}
